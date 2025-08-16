19c6b5c1feb7d679d555d3391b94903c02165a92
Atharva <atharvaraj@gmail.com>
2025-08-17 03:28:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { PipelineToolbar } from "./toolbar";
+import { PipelineUI } from "./ui";
+import { SubmitButton } from "./submit";
+
+function App() {
+  return (
+    <div>
+      <PipelineToolbar />
+      <PipelineUI />
+      hi
+      <SubmitButton />
+    </div>
+  );
+}
+
+export default App;
